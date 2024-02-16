81ac9131ba78ebcd67ec92cc03be535fd47201fa
Mohamed Ali <mohamed.ali300094@gmail.com>
2024-02-16 20:50:17 +0200

    change landing page structure

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,35 @@
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-        };
-    });
-};
+
+const nav=document.querySelector('.navbar')
+
+window.addEventListener('scroll',function(){
+    if(window.scrollY >= 56){
+        nav.classList.add('navbar-scrolled')
+    }else{
+        nav.classList.remove('navbar-scrolled')
+    }
+})
+
+const li=document.querySelectorAll(".nav-link");
+const sec=document.querySelectorAll("section");
+
+function activeMenu(){
+    let len = sec.length;
+    while (--len && window.scrollY + 97< sec[len].offsetTop){}
+    li.forEach(item => 
+        item.classList.remove("active"));
+        li[len].classList.add("active");
+}
+activeMenu()
+window.addEventListener("scroll",activeMenu);
+
+
+AOS.init({
+    
+  offset: 120,
+  delay: 0,
+  duration: 900,
+  easing: 'ease',
+  once: false,
+  mirror: false,
+  anchorPlacement: 'top-bottom',
+});
